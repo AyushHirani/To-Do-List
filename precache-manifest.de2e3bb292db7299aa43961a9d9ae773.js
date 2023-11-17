cdc262d8adc28c7f020d1acfa1741dcea1ad582e
Ayush <130111400+AyushHirani@users.noreply.github.com>
2023-11-17 03:18:23 +0000

    Updates

---
+++ precache-manifest.de2e3bb292db7299aa43961a9d9ae773.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0cb631dfb60e797a536c67e8ba2037c",
+    "url": "/To-Do-List/index.html"
+  },
+  {
+    "revision": "a2d12905c0b569165bba",
+    "url": "/To-Do-List/static/js/2.61ae90a2.chunk.js"
+  },
+  {
+    "revision": "6ded8fbf1a6347b3da0d",
+    "url": "/To-Do-List/static/js/main.c61ac2fa.chunk.js"
+  },
+  {
+    "revision": "9dd0751940dee3b918bf",
+    "url": "/To-Do-List/static/js/runtime-main.d1897ef9.js"
+  }
+]);
